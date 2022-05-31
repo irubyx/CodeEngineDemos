6302fce473548a3a81cdc3369e1f395eddae9d9a
Sebastian Fripp <80719326+irubyx@users.noreply.github.com>
2022-05-31 19:19:56 -0300

    Update server.js

--- hello-world/server.js
+++ hello-world/server.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0"
 // App
 const app = express()
 app.get("/", (req, res) => {
-    const msg = process.env.person ? process.env.person : 'world'
+    const msg = process.env.persona ? process.env.persona : 'world'
     res.send(`Hello ${msg}!`)
 })
 

ed398722bd126333f8f9397f253e7255d41827b9
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-07-22 12:44:46 -0300

    Update server.js

--- service-binding/server.js
+++ service-binding/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.get("/", (req, res) => {
     if (process.env.CE_SERVICES) {
         let ce_services = JSON.parse(process.env.CE_SERVICES)
-        res.send("Estos son los servicios vinculados: " + ce_services[0])
+        res.send("Estos son los servicios vinculados: " + ce_services["cloud-object-storage"][0])
     } else {
         const msg = "No existe CE_SERVICES"
         res.send(msg)
